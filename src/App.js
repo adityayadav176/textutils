eed530bfd92f53d8b381211f2e656bd837667bfb
adityayadav176 <ay6598aditya@gmail.com>
2026-01-06 21:43:13 +0530

    add alert component

--- src/App.js
+++ src/App.js
@@ -4,31 +4,43 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 
-
 function App() {
-  const [mode, setMode] = useState('mode')
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
-      setMode('dark')
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    // Auto remove alert after 2 sec
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
     }
-    else{
-      setMode('light')
-      document.body.style.backgroundColor = 'white'
-    }
-  }
+  };
 
   return (
     <>
-  <Navbar title="textUtils" toggleMode={toggleMode} mode={mode}/>
-  <Alert alert="this is alert"/>
-  
-  <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below" mode={mode}/>
-    {/* <About/> */}
-  </div>
-  </>
+      <Navbar title="TextUtils" toggleMode={toggleMode} mode={mode} />
+      <Alert alert={alert} />
+
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" mode={mode} />
+      </div>
+    </>
   );
 }
 
