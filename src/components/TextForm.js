febc22e6df0e877376c9a2c3963b52b1e081fc6d
adityayadav176 <ay6598aditya@gmail.com>
2026-01-06 23:52:22 +0530

    bug fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -39,7 +39,7 @@ export default function TextForm(props) {
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} onChange={handleOnChange} id="my-box" rows="8"></textarea>
+                    <textarea className="form-control" spellCheck="false" value={text} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} onChange={handleOnChange} id="my-box" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary my-3 mx-2" onClick={handleUpClick}> Convert to Uppercase </button>
                 <button className="btn btn-primary my-3 mx-2" onClick={handleLoClick}> Convert to lowercase </button>
